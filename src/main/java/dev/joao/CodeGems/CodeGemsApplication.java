d70699d85ca4f2401a7360a7828c1ef8ac0ea515
Joao Henrique <joaoibitira@gmail.com>
2023-05-25 17:50:16 -0300

    created repository

---
+++ src/main/java/dev/joao/CodeGems/CodeGemsApplication.java
@@ -0,0 +1,13 @@
+package dev.joao.CodeGems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeGemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeGemsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/joao/CodeGems/CodeGemsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.joao.CodeGems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeGemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
